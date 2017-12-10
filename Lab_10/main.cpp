2131e7270b6501266df954965869b8b9c799a84c
Rustam Yunusov <yrustt@yandex.ru>
2017-12-11 02:22:46 +0300

    -

--- Lab_10/main.cpp
+++ Lab_10/main.cpp
@@ -19,8 +19,8 @@ typedef uint64_t index_type;
 
 index_type MAX_VALUE = UINT64_MAX;
 
-index_type BLOCK_SIZE = 150;
-index_type MEMORY_SIZE = 750;
+index_type BLOCK_SIZE = 100;
+index_type MEMORY_SIZE = 700;
 index_type COUNT_BLOCK = MEMORY_SIZE / BLOCK_SIZE;
 
 template<index_type size>
